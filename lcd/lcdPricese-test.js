016f519a7f745ad37296bbd63ba58acc3caaf07c
douqing <15829258737@163.com>
2016-05-06 18:28:42 +0800

    second modified

--- lcd/lcdPricese-test.js
+++ lcd/lcdPricese-test.js
@@ -3,9 +3,35 @@
  */
 describe('lcdPricese', function() {
     var input;
-
     beforeEach(function() {
         input=910;
+    });
+
+    it('getNumberArray should output correct text',function () {
+        var lcdNumbers=loadDigits();
+        spyOn(console,"log");
+        getNumberArray(input.toString(),lcdNumbers);
+        var expectText = [
+            ['._.','|_|','..|'],
+            ['...','..|','..|'],
+            ['._.','|.|','|_|']
+        ];
+        expect(console.log).toHaveBeenCalledWith(expectText);
+    });
+
+    it('drawingPattern should output correct text',function () {
+        var numberArray = [
+            ['._.','|_|','..|'],
+            ['...','..|','..|'],
+            ['._.','|.|','|_|']
+        ];
+        spyOn(console,"log");
+        drawingPattern(numberArray);
+        var expectText= [['._.','|_|','..|'],
+            ['...','..|','..|'],
+            ['._.','|.|','|_|']];
+        expect(console.log).toHaveBeenCalledWith(expectText);
+
     });
 
     it('should print correct text', function() {
@@ -14,15 +40,10 @@ describe('lcdPricese', function() {
 
         printText(input);
 
-        var expectText ='._.'+'\n'+
-                        '|_|'+'\n'+
-                        '..|'+
-                        '...' + '\n' +
-                        '..|' + '\n' +
-                        '..|'+
-                        '._.' + '\n' +
-                        '|.|' + '\n' +
-                        '|_|';
+        var expectText ='._.'+'...'+'._.'+'\n'+
+                        '|_|'+'..|'+'|.|'+'\n'+
+                        '..|'+'..|'+'|_|'+'\n';
+
 
         expect(console.log).toHaveBeenCalledWith(expectText);
     });
--- lcd/lcdPricese.js
+++ lcd/lcdPricese.js
@@ -1,29 +1,39 @@
 
-function  printText(input) {
-    var numStrings=numberToString(input);
+function  printText(inputs) {
+    var numberStrings=inputs.toString();
     var lcdNumbers=loadDigits();
-    var number=outputNumber(numStrings,lcdNumbers);
-    console.log(number);
+    var numberArray=getNumberArray(numberStrings,lcdNumbers);
+    var pattern=drawingPattern(numberArray);
+    console.log(pattern);
 }
 
-function numberToString(number) {
-   return  number.toString();
-}
-
-function outputNumber(numStrings,lcdNumbers) {
-    var result='';
-    for(var i = 0;i < numStrings.length; i++){
-      var it= isEqual(numStrings[i],lcdNumbers);
-       if(it){
-           result+=it;
-       }
-   }
-    return result;
-}
-function isEqual(numString, lcdNumbers) {
-    for (var i = 0;i < lcdNumbers.length;i ++){
-       if(parseInt(numString) === (lcdNumbers[i].number)[0]){
-           return (lcdNumbers[i].number)[1];
-       }
+function getNumberArray(numberStrings, lcdNumbers) {
+    var numberArray=[];
+   for(var i = 0;i <numberStrings.length;i ++){
+       var numberString=numberStrings.charAt(i);
+       var lcdNumber=isExist(numberString,lcdNumbers);
+        numberArray.push(lcdNumber)
     }
+    return numberArray;
+}
+
+function isExist(numberString,lcdNumbers) {
+    var lcdNumber=[];
+    var input=parseInt(numberString);
+    if(input<lcdNumbers.length){
+        lcdNumber=lcdNumbers[input];
+    }
+    return lcdNumber;
+
+}
+
+function drawingPattern(numberArray) {
+    var pattern='';
+    for (var k = 0;k< 3;k ++){
+        for(var i = 0;i < numberArray.length; i ++){
+            pattern+=numberArray[i][k];
+        }
+        pattern+='\n';
+    }
+    return pattern;
 }
